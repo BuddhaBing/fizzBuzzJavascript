e9b2d3b323346458ee3d1243bd78132e357ae699
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-30 15:57:52 +0000

    fizzbuzz complete

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,19 +1,22 @@
 describe('FizzBuzz', function() {
-    
+
     var fizzbuzz;
-    
+
     beforeEach(function() {
         fizzbuzz = new FizzBuzz();
     });
-    
+
     it("should output fizz when given a number that is divisible by 3", function() {
         expect(fizzbuzz.play(3)).toEqual("Fizz");
     });
-    
+
     it("should output buzz when given a number that is divisible by 5", function() {
-    
+        expect(fizzbuzz.play(5)).toEqual("Buzz");
     });
     it("should output fizzbuzz when given a number that is divisible by 3 and 5", function() {
-    
+        expect(fizzbuzz.play(15)).toEqual("FizzBuzz");
+    });
+    it("should return the number when given a number that is not divisible by 3 or 5", function() {
+        expect(fizzbuzz.play(37)).toEqual(37);
     });
 })
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,7 +2,20 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.play = function(number) {
-    if (number % 3 === 0) {
+    if (this._isDivisibleBy(15, number)) {
+        return "FizzBuzz";
+    }
+    else if (this._isDivisibleBy(3, number)) {
         return "Fizz";
     }
-}
+    else if (this._isDivisibleBy(5, number)) {
+        return "Buzz";
+    }
+    else {
+        return number;
+    }
+};
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+    return number % divisor === 0;
+};
